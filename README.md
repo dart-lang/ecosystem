f8744879efd4955442dffa542614e421c2ba3fbd
Devon Carew <devoncarew@google.com>
2022-10-25 17:53:52 -0700

    commit an initial version of package:dash_team_lints

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 [![Dart](https://github.com/dart-lang/ecosystem/actions/workflows/dart.yaml/badge.svg)](https://github.com/dart-lang/ecosystem/actions/workflows/dart.yaml)
 
-A tool to calculate the API usage for a package.
+## Overview
 
-## Projects docs
+This repository is home to general Dart Ecosystem tools and packages.
 
-todo: add a brief project description here
+## Packages
+
+Package | Description | Published Version
+--- | --- | ---
+[corpus](packages/corpus/) | A tool to calculate the API usage for a package |
+[dash_team_lints](packages/dash_team_lints/) | An analysis rule set used by the Dart and Flutter teams |
---
+++ packages/dash_team_lints/test/validate_test.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart' as yaml;
+
+void main() {
+  late String content;
+
+  setUp(() {
+    return content = File('lib/analysis_options.yaml').readAsStringSync();
+  });
+
+  test('well-formed', () {
+    var result = yaml.loadYaml(content);
+    expect(result, isMap);
+  });
+
+  test('references recommended', () {
+    var result = yaml.loadYaml(content);
+    expect(result['include'], equals('package:lints/recommended.yaml'));
+  });
+
+  test('defines linter rules', () {
+    var result = yaml.loadYaml(content);
+    expect(result['linter']['rules'], isNotEmpty);
+  });
+}
