1893239686a816989c1d55d91cef569b02ae6f22
Devon Carew <devoncarew@gmail.com>
2023-01-18 19:30:23 -0800

    determine the default branch for the auto-publish fix

---
+++ pkgs/blast_repo/test/utils_test.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io' as io;
+
+import 'package:blast_repo/src/utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('gitDefaultBranch', () {
+    final result = gitDefaultBranch(io.Directory.current.parent.parent);
+
+    expect(result, 'main');
+  });
+}
