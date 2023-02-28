b4b1c68400eeaddf2ff2a7f9c884987fd58342d3
Devon Carew <devoncarew@google.com>
2023-02-27 18:45:34 -0800

    support an 'publish-ignore-warnings' label (#77)
    * support an 'publish-ignore-warnings' label
    
    * make the 'use label' message more prominent
    
    * remove warning

--- pkgs/firehose/lib/src/github.dart
+++ pkgs/firehose/lib/src/github.dart
@@ -27,6 +27,10 @@ class Github {
   /// The PR (or issue) number.
   String? get issueNumber => _env['ISSUE_NUMBER'];
 
+  /// Any labels applied to this PR.
+  List<String> get prLabels =>
+      _env.containsKey('PR_LABELS') ? _env['PR_LABELS']!.split(',') : [];
+
   /// The commit SHA that triggered the workflow.
   String? get sha => _env['GITHUB_SHA'];
 
@@ -190,6 +194,11 @@ class Github {
 
   void close() {
     _httpClient?.close();
+  }
+
+  /// Write a notice message to the github log.
+  void notice({required String message}) {
+    print('::notice ::$message');
   }
 }
 
