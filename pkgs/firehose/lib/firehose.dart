61a5b90a8f4d22e5c9bf0f2391b26dcf01cec4fc
Moritz <mosum@google.com>
2023-06-26 11:00:08 -0400

    fix formatting in details

--- pkgs/firehose/lib/firehose.dart
+++ pkgs/firehose/lib/firehose.dart
@@ -196,9 +196,15 @@ Documentation at https://github.com/dart-lang/ecosystem/wiki/Publishing-automati
   ) async {
     var commentText = results.map((e) {
       var markdown = e.markdown;
-      var s = e.severity == Severity.error
-          ? markdown
-          : '<details><summary>Details</summary>$markdown</details>';
+      var s = '''
+<details>
+<summary ${e.severity == Severity.error ? 'open' : ''}>
+
+Details
+</summary>
+
+$markdown
+</details>''';
       return '${e.tag} ${e.severity.emoji}\n\n$s';
     }).join('\n');
 
