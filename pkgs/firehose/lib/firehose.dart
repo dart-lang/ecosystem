f40a4eb09524ccc223fa959a18bf37487ee27147
Devon Carew <devoncarew@google.com>
2023-09-25 16:13:13 -0700

    improve support for '-dev' and '-wip' package versions (#173)
    * improve support for '-dev' and '-wip' package versions
    
    * update the workflow files
    
    * try out a -dev version
    
    * rev to a stable version

--- pkgs/firehose/lib/firehose.dart
+++ pkgs/firehose/lib/firehose.dart
@@ -138,11 +138,8 @@ Saving existing comment id $existingCommentId to file ${idFile.path}''');
         var result = Result.info(package, 'already published at pub.dev');
         print(result);
         results.addResult(result);
-      } else if (package.pubspec.version!.isPreRelease) {
-        var result = Result.info(
-          package,
-          'pre-release version (no publish necessary)',
-        );
+      } else if (package.pubspec.version!.wip) {
+        var result = Result.info(package, 'WIP (no publish necessary)');
         print(result);
         results.addResult(result);
       } else {
