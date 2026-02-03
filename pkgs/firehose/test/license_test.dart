e72c81bd15e51f5fec24f15ebf9079a0e1538ffb
Moritz <mosum@google.com>
2026-02-03 14:47:12 +0100

    add test to make sure

--- pkgs/firehose/test/license_test.dart
+++ pkgs/firehose/test/license_test.dart
@@ -13,25 +13,42 @@ import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 void main() {
-  final fileWithLicense = File('test/fileWithLicense.dart');
-  final fileWithoutLicense = File('test/fileWithoutLicense.dart');
-  final licenseOptions = LicenseOptions();
+  late File fileWithLicense;
+  late File fileWithoutLicense;
+  late Directory directory;
 
-  setUp(() async {
-    await fileWithLicense.writeAsString(licenseOptions.license);
+  setUpAll(() async {
+    directory = Directory.systemTemp.createTempSync('license_test');
+    fileWithLicense = File(path.join(directory.path, 'fileWithLicense.dart'));
+    fileWithoutLicense =
+        File(path.join(directory.path, 'fileWithoutLicense.dart'));
     await fileWithoutLicense.writeAsString('');
   });
 
-  test('Check for licenses', () async {
-    final directory = Directory('test/');
+  test('Check for license', () async {
+    final licenseOptions = LicenseOptions();
+    await fileWithLicense.writeAsString(licenseOptions.license);
+
     final filesWithoutLicenses = await getFilesWithoutLicenses(
         directory, [], licenseOptions.licenseTestString);
     expect(filesWithoutLicenses,
         [path.relative(fileWithoutLicense.path, from: directory.path)]);
   });
 
-  tearDown(() async {
-    await fileWithLicense.delete();
-    await fileWithoutLicense.delete();
+  test('Check for custom license', () async {
+    final licenseOptions = LicenseOptions(license: '''
+// My custom license
+// 
+// Dabadu''', licenseTestString: '// My ');
+    await fileWithLicense.writeAsString(licenseOptions.license);
+
+    final filesWithoutLicenses = await getFilesWithoutLicenses(
+        directory, [], licenseOptions.licenseTestString);
+    expect(filesWithoutLicenses,
+        [path.relative(fileWithoutLicense.path, from: directory.path)]);
+  });
+
+  tearDownAll(() async {
+    await directory.delete(recursive: true);
   });
 }
