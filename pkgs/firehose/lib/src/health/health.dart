13a9ad9e10f268857c09d71801573e3fe15e10e3
Kevin Moore <kevmoo@google.com>
2026-02-04 19:23:59 -0800

    firehose: echo any error output from dependencies task on failure
    Also fix health workflow to NOT install flutter unless it's needed
    
    Fixes https://github.com/dart-lang/ecosystem/issues/392

--- pkgs/firehose/lib/src/health/health.dart
+++ pkgs/firehose/lib/src/health/health.dart
@@ -204,7 +204,7 @@ class Health {
         logStdout: false,
       );
 
-      final (result, _, _) = runDashProcess(
+      final (result, out, err) = runDashProcess(
         flutterPackages,
         package,
         [
@@ -219,7 +219,7 @@ class Health {
 
       if (result.exitCode != 0) {
         hasError = true;
-        final output = (result.stderr as String).replaceAll('\n', '<br>');
+        final output = '$out\\n$err'.trim().replaceAll('\\n', '<br>');
         results.add('''
 | ${package.name} | <details><summary>:exclamation: Show Issues</summary><pre>$output</pre></details> |''');
       } else {
